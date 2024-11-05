b2d363acffad232f42edc3ed8ca93afc39f3f216
Devin Jean <emailcruzjean@yahoo.com>
2024-11-05 12:30:33 -0600

    update cli

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@ fn main() {
     if args.len() != 2 {
         panic!("usage: {} [input path]", args[0]);
     }
-    let input = std::fs::read(&args[1]).unwrap();
-    let composition = amm_sdk::storage::Storage::AMM.load_data(&input).unwrap();
+
+    let content = std::fs::read(&args[1]).unwrap();
+
+    let composition = match args[1].rsplit('.').next().unwrap_or_default() {
+        "musicxml" => amm_sdk::storage::Storage::MusicXML.load_data(&content).unwrap(),
+        "mid" | "smf" => amm_sdk::storage::Storage::MIDI.load_data(&content).unwrap(),
+        _ => amm_sdk::storage::Storage::AMM.load_data(&content).unwrap(),
+    };
+
     println!("{}", amm_sdk_netsblox::translate(&composition).unwrap());
 }
